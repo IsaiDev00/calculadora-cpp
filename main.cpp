6329f366355324285d2e527b70748ceade7cbcd0
Joshua Romo <joshua.romo@outlook.com>
2025-06-14 19:08:10 -0600

    Revert "Error en módulo"
    This reverts commit a937d43a1fcb1ab1af8a74bfd13117123a89a738.

--- main.cpp
+++ main.cpp
@@ -2,23 +2,16 @@
 
 using namespace std;
 
-int suma(int a, int b)
-{
+int suma(int a, int b) {
     return a + b;
 }
-int division(int a, int b)
-{
+int division(int a, int b) {
     return b != 0 ? a / b : 0;
 }
-int modulo(int a, int b)
-{
-    return a + b; // ERROR
-}
 
-int main()
-{
+int main() {
     cout << "Suma: " << suma(4, 2) << endl;
     cout << "División: " << division(4, 2) << endl;
-    cout << "Modulo: " << modulo(4, 2) << endl;
+
     return 0;
 }
