3d9d510eb1708daa1f49e8ce6d74968489803690
Fer <chitito_fresa@hotmail.com>
2025-06-15 15:16:29 -0600

    Agregue funcion resta

--- main.cpp
+++ main.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
-int main() {
+int suma(int a, int b)
+{
+    return a + b;
+}
+int resta(int a, int b)
+{
+    return a - b:
+}
+int division(int a, int b)
+{
+    return b != 0 ? a / b : 0;
+}
+int modulo(int a, int b)
+{
+    return a % b;
+}
+
+int main()
+{
+    cout << "Suma: " << suma(4, 2) << endl;
+    cout << "resta: " << resta(4, 2) << endl;
+    cout << "División: " << division(4, 2) << endl;
+    cout << "Modulo: " << modulo(4, 2) << endl;
 
     return 0;
 }
