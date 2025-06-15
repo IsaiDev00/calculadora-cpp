367f878ea5bce5c0ba842b39e5aab7698cd90fe5
Fernando Guzman <a25110136@ceti.mx>
2025-06-14 18:21:46 -0600

    revision 1

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@ using namespace std;
 int suma(int a, int b) {
     return a + b;
 }
+int division(int a, int b) {
+    return a / 0;  // ERROR
+}
 
 int main() {
     cout << "Suma: " << suma(4, 2) << endl;
+    cout << "División: " << division(4, 2) << endl;
+
     return 0;
 }
